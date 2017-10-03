ab96cd9e6c252a9583684da154cfda13813a1d38
LuisTei17 <luisedteixeira@gmail.com>
2017-10-03 20:47:27 -0300

    ''

--- src/main/java/com/OtauBanco/Application.java
+++ src/main/java/com/OtauBanco/Application.java
@@ -20,3 +20,13 @@ public class Application extends SpringBootServletInitializer {
     	SpringApplication.run(Application.class, args);
     }
 }
+
+/*
+ * spring.jpa.hibernate.ddl-auto=update
+spring.datasource.url=jdbc:mysql://sql10.freemysqlhosting.net/sql10197603
+spring.datasource.username=sql10197603
+spring.datasource.password=Q4sbq9HhTE
+spring.datasource.driver-class-name=com.mysql.jdbc.Driver
+spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL5Dialect
+*/
